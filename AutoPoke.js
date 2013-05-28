b30579a00c4f1f7c97c5dbcb3672570a6cab6999
Maxime Cyr <max_maxime_max@hotmail.com>
2013-05-28 15:59:10 -0400

    Première version

---
+++ AutoPoke.js
@@ -0,0 +1,72 @@
+if(document.getElementById('AutoPokeHeader') != null)
+	alert('AutoPoke est déjà installé sur la page');
+else
+{
+	var AutoPoke_Timer;
+	var AutoPoke_Running = false;
+
+	function AutoPoke_Verif()
+	{
+		console.log('Checking pokes');
+
+		var PokeList = document.querySelectorAll('.pokesDashboard .uiIconText');
+		for(var i = 0 ; i < PokeList.length ; i++)
+		{
+			console.log('Clicking one poke');
+			var Poke = PokeList[i];
+			Poke.click();
+		}
+
+		AutoPoke_Timer = setTimeout(AutoPoke_Verif, 2000);
+	}
+
+	function AutoPoke_Start()
+	{
+		AutoPoke_Verif();
+	}
+
+	function AutoPoke_Stop()
+	{
+		window.clearTimeout(AutoPoke_Timer);
+	}
+
+	function AutoPoke_SwitchStatus()
+	{
+		if(!AutoPoke_Running)
+		{
+			document.getElementById('AutoPokeStatus').innerHTML = "en marche";
+			document.getElementById('AutoPokeButton').innerHTML = "Arrêter";
+			AutoPoke_Start();
+			AutoPoke_Running = true;
+		}
+		else
+		{
+			document.getElementById('AutoPokeStatus').innerHTML = "hors fonction";
+			document.getElementById('AutoPokeButton').innerHTML = "Démarrer";
+			AutoPoke_Stop();
+			AutoPoke_Running = false;
+		}
+	}
+
+	function AutoPoke_Remove()
+	{
+		if(AutoPoke_Running) AutoPoke_Stop();
+		var Elem = document.getElementById('AutoPokeHeader');
+		Elem.parentNode.removeChild(Elem);
+	}
+
+	var Title = document.querySelector('h2.uiHeaderTitle');
+
+	var AutoPokeHeader = document.createElement("span");
+	AutoPokeHeader.id = "AutoPokeHeader";
+	AutoPokeHeader.style.backgroundColor = 'grey';
+	AutoPokeHeader.style.padding = 5+'px';
+	AutoPokeHeader.style.marginLeft = 10+'px';
+
+	AutoPokeHeader.innerHTML = "<span>AutoPoke <span id='AutoPokeStatus'>hors fonction</span></span> : <button id='AutoPokeButton' type='button'>Démarrer</button> (v0.1) <a href='#' id='AutoPokeRemoveButton'>&times;</a>";
+
+	Title.appendChild(AutoPokeHeader);
+
+	document.getElementById('AutoPokeButton').onclick = AutoPoke_SwitchStatus;
+	document.getElementById('AutoPokeRemoveButton').onclick = AutoPoke_Remove;
+}
